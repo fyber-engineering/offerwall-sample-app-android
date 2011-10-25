aa790c0c89c1a4f41f29ba55d31b5062bda54141
David Dávila <david@debugtheworld.com>
2011-10-25 15:40:56 +0200

    Refactored, improved UI layout, added exception catching and notification functionality, linked main activity's onCreate hook with the sending of the Advertiser Callback.

--- src/com/sponsorpay/sdk/android/testapp/SponsorpayAndroidTestAppActivity.java
+++ src/com/sponsorpay/sdk/android/testapp/SponsorpayAndroidTestAppActivity.java
@@ -2,6 +2,7 @@ package com.sponsorpay.sdk.android.testapp;
 
 import java.util.EnumMap;
 
+import com.sponsorpay.sdk.android.SponsorPay;
 import com.sponsorpay.sdk.android.advertiser.SponsorPayAdvertiser;
 import com.sponsorpay.sdk.android.publisher.SponsorPayPublisher;
 import com.sponsorpay.sdk.android.publisher.InterstitialLoader.InterstitialLoadingStatusListener;
@@ -14,6 +15,9 @@ import com.sponsorpay.sdk.android.testapp.R;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,23 +29,44 @@ import android.widget.TextView;
  * Example activity in order to show the usage of Sponsorpay Android SDK.
  */
 public class SponsorpayAndroidTestAppActivity extends Activity {
-	/**
-	 * Used to enclose user provided parameters from the state of the UI fields and check boxes in order to configure
-	 * the launch of the Offer Wall or Interstitial.
-	 */
-	private class LaunchOptions {
-		public String overridenAppId;
-		public String userId;
-		public boolean shouldStayOpen;
-		public boolean shouldUseStagingUrls;
-		public String backgroundUrl;
-		public String skinName;
-		public String securityToken;
-		public int callDelay;
-	}
+
+	private static final String APP_ID_PREFS_KEY = "APP_ID";
+	private static final String USER_ID_PREFS_KEY = "USER_ID";
+	private static final String KEEP_OFFERWALL_OPEN_PREFS_KEY = "KEEP_OFFERWALL_OPEN";
+	private static final String SKIN_NAME_PREFS_KEY = "SKIN_NAME";
+	private static final String BACKGROUND_URL_PREFS_KEY = "BACKGROUND_URL";
+	private static final String SECURITY_TOKEN_PREFS_KEY = "SECURITY_TOKEN";
+	private static final String DELAY_PREFS_KEY = "DELAY";
+	private static final String USE_STAGING_URLS_PREFS_KEY = "USE_STAGING_URLS";
 
 	private static final int DEFAULT_DELAY_MIN = 15;
-	
+	private static final String DEFAULT_SECURITY_TOKEN_VALUE = "b6acfc0227c82edb41f118d638e3c1d4e9ce0189";
+
+	/**
+	 * Shared preferences file name. Stores the values entered into the UI fields.
+	 */
+	private static final String PREFERENCES_FILE_NAME = "SponsorPayAdvertiserState";
+
+	private String mOverridenAppId;
+	private String mUserId;
+	private boolean mShouldStayOpen;
+	private String mBackgroundUrl;
+	private String mSkinName;
+	private String mSecurityToken;
+	private int mCallDelay;
+
+	private EditText mAppIdField;
+	private EditText mUserIdField;
+	private CheckBox mKeepOfferwallOpenCheckBox;
+	private EditText mSkinNameField;
+	private EditText mBackgroundUrlField;
+	private EditText mSecurityTokenField;
+	private EditText mDelayField;
+
+	private CheckBox mUseStagingUrlsCheckBox;
+
+	private boolean mShouldSendAdvertiserCallbackOnResume;
+
 	/**
 	 * Called when the activity is first created. See {@link Activity}.
 	 */
@@ -51,14 +76,78 @@ public class SponsorpayAndroidTestAppActivity extends Activity {
 
 		setContentView(R.layout.main);
 
-		((TextView) findViewById(R.id.sdk_version_string)).setText("Publisher SDK v. "
-				+ SponsorPayPublisher.RELEASE_VERSION_STRING);
+		((TextView) findViewById(R.id.sdk_version_string)).setText("SponsorPay Android SDK v. "
+				+ SponsorPay.RELEASE_VERSION_STRING);
+
+		mAppIdField = (EditText) findViewById(R.id.app_id_field);
+		mUserIdField = (EditText) findViewById(R.id.user_id_field);
+		mKeepOfferwallOpenCheckBox = (CheckBox) findViewById(R.id.keep_offerwall_open_checkbox);
+		mSkinNameField = (EditText) findViewById(R.id.skin_name_field);
+		mBackgroundUrlField = (EditText) findViewById(R.id.background_url_field);
+		mSecurityTokenField = (EditText) findViewById(R.id.security_token_field);
+		mDelayField = (EditText) findViewById(R.id.delay_field);
+
+		mUseStagingUrlsCheckBox = (CheckBox) findViewById(R.id.use_staging_urls_checkbox);
 
 		setCustomErrorMessages();
+
+		mShouldSendAdvertiserCallbackOnResume = true;
+	}
+
+	@Override
+	protected void onPause() {
+		// Save the state of the UI fields into the app preferences.
+		fetchValuesFromFields();
+
+		SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+		Editor prefsEditor = prefs.edit();
+
+		prefsEditor.putString(APP_ID_PREFS_KEY, mOverridenAppId);
+		prefsEditor.putString(USER_ID_PREFS_KEY, mUserId);
+		prefsEditor.putBoolean(KEEP_OFFERWALL_OPEN_PREFS_KEY, mShouldStayOpen);
+		prefsEditor.putString(BACKGROUND_URL_PREFS_KEY, mBackgroundUrl);
+		prefsEditor.putString(SKIN_NAME_PREFS_KEY, mSkinName);
+		prefsEditor.putString(SECURITY_TOKEN_PREFS_KEY, mSecurityToken);
+		prefsEditor.putInt(DELAY_PREFS_KEY, mCallDelay);
+
+		prefsEditor.putBoolean(USE_STAGING_URLS_PREFS_KEY, mUseStagingUrlsCheckBox.isChecked());
+
+		prefsEditor.commit();
+
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		// Recover the state of the UI fields from the app preferences.
+		SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+
+		mOverridenAppId = prefs.getString(APP_ID_PREFS_KEY, "");
+		mUserId = prefs.getString(USER_ID_PREFS_KEY, "");
+		mShouldStayOpen = prefs.getBoolean(KEEP_OFFERWALL_OPEN_PREFS_KEY, true);
+		mBackgroundUrl = prefs.getString(BACKGROUND_URL_PREFS_KEY, "");
+		mSkinName = prefs.getString(SKIN_NAME_PREFS_KEY, "");
+		mSecurityToken = prefs.getString(SECURITY_TOKEN_PREFS_KEY, DEFAULT_SECURITY_TOKEN_VALUE);
+		mCallDelay = prefs.getInt(DELAY_PREFS_KEY, DEFAULT_DELAY_MIN);
+
+		setValuesInFields();
+
+		mUseStagingUrlsCheckBox.setChecked(prefs.getBoolean(USE_STAGING_URLS_PREFS_KEY, false));
+
+		if (mShouldSendAdvertiserCallbackOnResume) {
+			if (mOverridenAppId != null && !mOverridenAppId.equals(""))
+				sendAdvertiserCallback();
+			else
+				Log.w(SponsorpayAndroidTestAppActivity.class.getSimpleName(),
+						"No advertiser callback is being sent on application launch because App ID is empty.");
+			mShouldSendAdvertiserCallbackOnResume = false;
+		}
 	}
 
 	/**
-	 * Sets custom UI messages in the SDK to demonstrate the use of SponsorPayPublisher.setCustomUIStrings();
+	 * Sets one custom UI message in the SDK to demonstrate the use of SponsorPayPublisher.setCustomUIStrings();
 	 */
 	private void setCustomErrorMessages() {
 		EnumMap<UIStringIdentifier, Integer> customUIStrings = new EnumMap<UIStringIdentifier, Integer>(
@@ -68,41 +157,23 @@ public class SponsorpayAndroidTestAppActivity extends Activity {
 	}
 
 	/**
-	 * Fetches user provided options from the state of the UI text fields and text boxes.
-	 * 
-	 * @return A {@link LaunchOptions} instance filled with the retrieved data.
+	 * Fetches user provided values from the state of the UI text fields and text boxes.
 	 */
-	private LaunchOptions fetchLaunchOptions() {
-		EditText appIdField = (EditText) findViewById(R.id.app_id_field);
-		EditText userIdField = (EditText) findViewById(R.id.user_id_field);
-		CheckBox keepOfferwallOpenCheckBox = (CheckBox) findViewById(R.id.keep_offerwall_open_checkbox);
-		CheckBox useStagingUrlsCheckBox = (CheckBox) findViewById(R.id.use_staging_urls_checkbox);
-		EditText skinNameField = (EditText) findViewById(R.id.skin_name_field);
-		EditText backgroundUrlField = (EditText) findViewById(R.id.background_url_field);
-		EditText securityTokenField = (EditText) findViewById(R.id.security_token_field);
-		EditText delayField = (EditText) findViewById(R.id.delay_field);
+	private void fetchValuesFromFields() {
+		mOverridenAppId = mAppIdField.getText().toString();
+		mUserId = mUserIdField.getText().toString();
+		mShouldStayOpen = mKeepOfferwallOpenCheckBox.isChecked();
 
-		LaunchOptions lo = new LaunchOptions();
-		try {
-			lo.overridenAppId = appIdField.getText().toString();
-		} catch (NumberFormatException e) {
-			lo.overridenAppId = null;
-		}
-
-		lo.userId = userIdField.getText().toString();
-		lo.shouldStayOpen = keepOfferwallOpenCheckBox.isChecked();
-		lo.shouldUseStagingUrls = useStagingUrlsCheckBox.isChecked();
-
-		String skinNameValue = skinNameField.getText().toString();
+		String skinNameValue = mSkinNameField.getText().toString();
 		if (!skinNameValue.equals("")) {
-			lo.skinName = skinNameValue;
+			mSkinName = skinNameValue;
 		}
 
-		lo.backgroundUrl = backgroundUrlField.getText().toString();
-		lo.securityToken = securityTokenField.getText().toString();
+		mBackgroundUrl = mBackgroundUrlField.getText().toString();
+		mSecurityToken = mSecurityTokenField.getText().toString();
 
-		String delay = delayField.getText().toString();
-		Integer parsedInt = null;
+		String delay = mDelayField.getText().toString();
+		Integer parsedInt;
 
 		try {
 			parsedInt = Integer.parseInt(delay);
@@ -113,11 +184,25 @@ public class SponsorpayAndroidTestAppActivity extends Activity {
 			parsedInt = DEFAULT_DELAY_MIN;
 		}
 
-		delayField.setText(String.format("%d", parsedInt));
-		
-		lo.callDelay = parsedInt;
-		
-		return lo;
+		mDelayField.setText(String.format("%d", parsedInt));
+
+		mCallDelay = parsedInt;
+
+		SponsorPayAdvertiser.setShouldUseStagingUrls(mUseStagingUrlsCheckBox.isChecked());
+		SponsorPayPublisher.setShouldUseStagingUrls(mUseStagingUrlsCheckBox.isChecked());
+	}
+
+	/**
+	 * Sets values in the state of the UI text fields and text boxes.
+	 */
+	private void setValuesInFields() {
+		mAppIdField.setText(mOverridenAppId);
+		mUserIdField.setText(mUserId);
+		mKeepOfferwallOpenCheckBox.setChecked(mShouldStayOpen);
+		mSkinNameField.setText(mSkinName);
+		mBackgroundUrlField.setText(mBackgroundUrl);
+		mSecurityTokenField.setText(mSecurityToken);
+		mDelayField.setText(String.format("%d", mCallDelay));
 	}
 
 	/**
@@ -126,62 +211,85 @@ public class SponsorpayAndroidTestAppActivity extends Activity {
 	 * @param v
 	 */
 	public void onLaunchOfferwallClick(View v) {
-		LaunchOptions lo = fetchLaunchOptions();
-
-		SponsorPayPublisher.setShouldUseStagingUrls(lo.shouldUseStagingUrls);
+		fetchValuesFromFields();
 
 		startActivityForResult(
 		/* Pass in a User ID */
-		SponsorPayPublisher.getIntentForOfferWallActivity(getApplicationContext(), lo.userId, lo.shouldStayOpen,
-				lo.overridenAppId), SponsorPayPublisher.DEFAULT_OFFERWALL_REQUEST_CODE);
+		SponsorPayPublisher.getIntentForOfferWallActivity(getApplicationContext(), mUserId, mShouldStayOpen,
+				mOverridenAppId), SponsorPayPublisher.DEFAULT_OFFERWALL_REQUEST_CODE);
 	}
 
+	/**
+	 * Sends the advertiser callback after fetching the values entered in the UI fields.
+	 */
+	private void sendAdvertiserCallback() {
+		fetchValuesFromFields();
+
+		try {
+			SponsorPayAdvertiser.register(getApplicationContext(), mOverridenAppId);
+		} catch (RuntimeException ex) {
+			showCancellableAlertBox("Exception from SDK", ex.getMessage());
+		}
+	}
+
+	/**
+	 * Invoked when the user clicks on the "Send advertiser callback now" button.
+	 * 
+	 * @param v
+	 */
 	public void onSendCallbackNowButtonClick(View v) {
-		LaunchOptions lo = fetchLaunchOptions();
-
-		SponsorPayAdvertiser.register(getApplicationContext(), lo.overridenAppId);
+		sendAdvertiserCallback();
 	}
 
+	/**
+	 * Invoked when the user clicks on the "Send advertiser callback with delay" button.
+	 * 
+	 * @param v
+	 */
 	public void onSendCallbackWithDelayButtonClick(View v) {
-		LaunchOptions lo = fetchLaunchOptions();
-
-		SponsorPayAdvertiser.registerWithDelay(getApplicationContext(), lo.callDelay, lo.overridenAppId);
+		fetchValuesFromFields();
+		try {
+			SponsorPayAdvertiser.registerWithDelay(getApplicationContext(), mCallDelay, mOverridenAppId);
+		} catch (RuntimeException ex) {
+			showCancellableAlertBox("Exception from SDK", ex.getMessage());
+		}
 	}
-	
+
 	/**
 	 * Triggered when the user clicks on the launch interstitial button.
 	 * 
 	 * @param v
 	 */
 	public void onLaunchInsterstitialClick(View v) {
-		Log.d("SP", "Launch Interstitial Clicked");
-		LaunchOptions lo = fetchLaunchOptions();
+		fetchValuesFromFields();
 
-		SponsorPayPublisher.setShouldUseStagingUrls(lo.shouldUseStagingUrls);
+		try {
+			SponsorPayPublisher.loadShowInterstitial(this, mUserId, new InterstitialLoadingStatusListener() {
 
-		SponsorPayPublisher.loadShowInterstitial(this, lo.userId, new InterstitialLoadingStatusListener() {
+				@Override
+				public void onInterstitialLoadingTimeOut() {
+					Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onInterstitialLoadingTimeOut");
+				}
 
-			@Override
-			public void onInterstitialLoadingTimeOut() {
-				Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onInterstitialLoadingTimeOut");
-			}
+				@Override
+				public void onInterstitialRequestError() {
+					Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onInterstitialRequestError");
+				}
 
-			@Override
-			public void onInterstitialRequestError() {
-				Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onInterstitialRequestError");
-			}
+				@Override
+				public void onNoInterstitialAvailable() {
+					Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onNoInterstitialAvailable");
+				}
 
-			@Override
-			public void onNoInterstitialAvailable() {
-				Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onNoInterstitialAvailable");
-			}
+				@Override
+				public void onWillShowInterstitial() {
+					Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onWillShowInterstitial");
+				}
 
-			@Override
-			public void onWillShowInterstitial() {
-				Log.d(SponsorpayAndroidTestAppActivity.class.toString(), "onWillShowInterstitial");
-			}
-
-		}, lo.shouldStayOpen, lo.backgroundUrl, lo.skinName, 0, lo.overridenAppId);
+			}, mShouldStayOpen, mBackgroundUrl, mSkinName, 0, mOverridenAppId);
+		} catch (RuntimeException ex) {
+			showCancellableAlertBox("Exception from SDK", ex.getMessage());
+		}
 	}
 
 	/**
@@ -192,11 +300,10 @@ public class SponsorpayAndroidTestAppActivity extends Activity {
 	 * @param v
 	 */
 	public void onRequestNewCoinsClick(View v) {
-		Log.d("SP", "Request New Coins Clicked");
+		fetchValuesFromFields();
 
-		LaunchOptions lo = fetchLaunchOptions();
-
-		final String usedTransactionId = VirtualCurrencyConnector.fetchLatestTransactionIdForUser(getApplicationContext(), lo.userId);
+		final String usedTransactionId = VirtualCurrencyConnector.fetchLatestTransactionIdForUser(
+				getApplicationContext(), mUserId);
 
 		SPCurrencyServerListener requestListener = new SPCurrencyServerListener() {
 
@@ -217,8 +324,12 @@ public class SponsorpayAndroidTestAppActivity extends Activity {
 			}
 		};
 
-		SponsorPayPublisher.requestNewCoins(getApplicationContext(), lo.userId, requestListener, null,
-				lo.securityToken, lo.overridenAppId);
+		try {
+			SponsorPayPublisher.requestNewCoins(getApplicationContext(), mUserId, requestListener, null,
+					mSecurityToken, mOverridenAppId);
+		} catch (RuntimeException ex) {
+			showCancellableAlertBox("Exception from SDK", ex.getMessage());
+		}
 	}
 
 	/**

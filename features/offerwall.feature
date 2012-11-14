d030c712ff42216ba82d9d3def2a885705cf519c
David Vicente <david.vicente@sponsorpay.com>
2012-11-14 14:16:50 +0100

    Offerwall visibility check step added

--- features/offerwall.feature
+++ features/offerwall.feature
@@ -1,10 +1,14 @@
 Feature: Offerwall feature
-    
-  Scenario: The user can launch the OfferWall
+  
+  Background:
     Given that user "tester" has valid credentials for "1246"
-	And that user launch the offerwall
+    
+  Scenario: The user can launch the OfferWall  
+	Given that user launch the offerwall
+	And that the offerwall is visible
 
   Scenario: Opening offerwall without credentials
+	And that the offerwall is visible
     Given that user launch the offerwall
 	
 
--- features/step_definitions/credentials_steps.rb
+++ features/step_definitions/credentials_steps.rb
@@ -1,25 +1,15 @@
-Given /^I am able to launch the application$/ do
-	element_exists("view")
-	sleep(STEP_PAUSE)
-end
-
-Given /^I am user "([^\"]*)"$/ do |name|
-	set_text("textField accessibilityLabel:'userIdTextField'", name)
-	sleep(STEP_PAUSE)
-end
-
 Given /^that user "([^\"]*)" has valid credentials for "([^\"]*)"$/ do |userId, appId|
-	if (! valid_credentials? userId,  appId)
-		enter_text( "#{appId}", "app_id_field")
-		enter_text( "#{userId}", "user_id_field")
-		performAction('press_button_with_text', 'Create new credentials')
-		if (! valid_credentials? userId,  appId)
-			screenshot_and_raise "Impossible to get valid credentials"
-		end	
-	end
+  if (! valid_credentials? userId,  appId)
+    enter_text( "#{appId}", "app_id_field")
+    enter_text( "#{userId}", "user_id_field")
+    performAction('press_button_with_text', 'Create new credentials')
+    if (! valid_credentials? userId,  appId)
+      screenshot_and_raise "Impossible to get valid credentials"
+    end  
+  end
 end
 
 def valid_credentials? (userId, appId)
-	credentialsInfo = performAction( 'get_view_property', 'credentials_info', 'text')["message"]
-	(credentialsInfo =~ /AppId - #{appId}\nUserId - #{userId}/) != nil 
+  credentialsInfo = performAction( 'get_view_property', 'credentials_info', 'text')["message"]
+  (credentialsInfo =~ /AppId - #{appId}\nUserId - #{userId}/) != nil 
 end

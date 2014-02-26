716f486752fe084c810b7227b9424429ec6d71d0
David Vicente <david.vicente@sponsorpay.com>
2014-02-26 14:56:42 +0100

    Added support for configurations and runtime metada for the request

--- sponsorpay-mediation-millenial/src/com/sponsorpay/mediation/interstitial/MillenialIntersitialMediationAdapter.java
+++ sponsorpay-mediation-millenial/src/com/sponsorpay/mediation/interstitial/MillenialIntersitialMediationAdapter.java
@@ -7,6 +7,13 @@
 package com.sponsorpay.mediation.interstitial;
 
 import java.lang.ref.WeakReference;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Context;
@@ -17,11 +24,18 @@ import com.millennialmedia.android.MMInterstitial;
 import com.millennialmedia.android.MMRequest;
 import com.millennialmedia.android.RequestListener;
 import com.sponsorpay.mediation.MillenialMediationAdapter;
+import com.sponsorpay.mediation.SPMediationConfigurator;
 import com.sponsorpay.publisher.interstitial.mediation.SPInterstitialMediationAdapter;
+import com.sponsorpay.utils.SponsorPayLogger;
 
 public class MillenialIntersitialMediationAdapter extends
 		SPInterstitialMediationAdapter<MillenialMediationAdapter> implements RequestListener  {
 
+	public static final String RUNTIME_METADATA_KEY = "runtimeMetadata";
+
+	private static final String TAG = "MillenialIntersitialMediationAdapter";
+	private static final String METADATA_KEY = "metadata";
+	
 	private MMInterstitial mInterstitial;
 
 	public MillenialIntersitialMediationAdapter(MillenialMediationAdapter adapter, Activity activity) {
@@ -33,6 +47,11 @@ public class MillenialIntersitialMediationAdapter extends
 		//Set your metadata in the MMRequest object
 		MMRequest request = new MMRequest();
 
+		Map<String, String> requestMetadata = new HashMap<String, String>();
+		requestMetadata.putAll(getConfigMetadata());
+		requestMetadata.putAll(getRuntimeMetadata());
+		request.setMetaValues(requestMetadata);
+		
 		//Add the MMRequest object to your MMInterstitial.
 		mInterstitial.setMMRequest(request);
 		mInterstitial.setListener(this);
@@ -93,6 +112,37 @@ public class MillenialIntersitialMediationAdapter extends
 		} else {
 			fireValidationErrorEvent(exception.getLocalizedMessage());
 		}
+	}
+	
+	// Helpers
+	private Map<String, String> getConfigMetadata() {
+		JSONObject metadata = SPMediationConfigurator.getConfiguration(getName(), METADATA_KEY, JSONObject.class);
+		if(metadata != null) {
+			try {
+				Map<String, String> map = new HashMap<String, String>(
+						metadata.length());
+				@SuppressWarnings("unchecked")
+				Iterator<String> itr = metadata.keys();
+				while (itr.hasNext()) {
+					String key = itr.next();
+					String value = metadata.getString(key);
+					map.put(key, value);
+				}
+				return map;
+			} catch (JSONException e) {
+				SponsorPayLogger.d(TAG, "Error parsing json - " + e.getLocalizedMessage() );
+			}
+		}
+		return Collections.emptyMap();
+	}
+	
+	@SuppressWarnings({ "unchecked" })
+	private Map<String, String> getRuntimeMetadata() {
+		Map<String, String> runtimeMetadata = SPMediationConfigurator.getConfiguration(getName(), RUNTIME_METADATA_KEY, HashMap.class);
+		if (runtimeMetadata != null) {
+			return runtimeMetadata;
+		}
+		return Collections.emptyMap();
 	}
 	
 }

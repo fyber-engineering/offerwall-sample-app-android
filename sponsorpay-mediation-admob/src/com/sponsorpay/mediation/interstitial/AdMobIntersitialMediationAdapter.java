8c75f25e14a3bcb807c4c8c1c8fbc4fe42454297
alexakios <alexios.karapetsas@sponsorpay.com>
2014-04-01 12:05:29 +0200

    We have integrated the settings for targeting ads on admob. We have included the gender, birthday and location targeting.

--- sponsorpay-mediation-admob/src/com/sponsorpay/mediation/interstitial/AdMobIntersitialMediationAdapter.java
+++ sponsorpay-mediation-admob/src/com/sponsorpay/mediation/interstitial/AdMobIntersitialMediationAdapter.java
@@ -6,11 +6,15 @@
 
 package com.sponsorpay.mediation.interstitial;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
@@ -32,6 +36,13 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 	private static final String BUILDER_CONFIG_ADD_TEST_DEVICE = "addTestDevice";
 	private static final String COOPA_COPLIANT                 = "isCOPPAcompliant";
 		
+	/**
+	 * The following keys are declared in the config file and are used
+	 * to set the runtime settings for the request adapter.
+	 */
+	private static final String BIRTHDAY_KEY   = "birthday";
+	private static final String GENDER_KEY     = "gender";
+	private static final String LOCATION_KEY   = "location";
 	
 	/**
 	 * These are the 4 possible errors that we can take back when we are 
@@ -41,6 +52,10 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 	private static final String INVALID_REQUEST = "ERROR_CODE_INVALID_REQUEST";
 	private static final String NETWORK_ERROR   = "ERROR_CODE_NETWORK_ERROR";
 	private static final String CODE_NO_FILL    = "ERROR_CODE_NO_FILL";
+	
+	private static Integer gender;
+	private static Date date;
+	private static Location location;
 	
 	
 	private InterstitialAd interstitial;
@@ -64,7 +79,7 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 			@Override
 			public void onAdClosed() {
 				super.onAdClosed();
-				SponsorPayLogger.i(TAG, "Return to the application after clicking on an ad.");
+				SponsorPayLogger.i(TAG, "Ad closed.");
 				
 				fireCloseEvent();
 			}
@@ -106,7 +121,7 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 				super.onAdLeftApplication();
 				
 				fireClickEvent();
-				SponsorPayLogger.i(TAG, "User leaves the application.");
+				SponsorPayLogger.i(TAG, "User leaves the application. Clicked on the ad.");
 			}
 
 			/**
@@ -166,6 +181,28 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 			}
 		}
 		
+		
+		//set all the runtime configuration for gender, location and date
+		//which will be used for ad targeting
+		
+		Map<String, Object> runtimeConfigurationForAdapter = getRuntimeMetadata();
+		if (!runtimeConfigurationForAdapter.isEmpty()) {
+			if (runtimeConfigurationForAdapter.containsKey(GENDER_KEY)) {
+
+				requestBuilder.setGender((Integer) runtimeConfigurationForAdapter.get(GENDER_KEY));
+			}
+
+			if (runtimeConfigurationForAdapter.containsKey(BIRTHDAY_KEY)) {
+
+				requestBuilder.setBirthday((Date) runtimeConfigurationForAdapter.get(BIRTHDAY_KEY));
+			}
+
+			if (runtimeConfigurationForAdapter.containsKey(LOCATION_KEY)) {
+
+				requestBuilder.setLocation((Location) runtimeConfigurationForAdapter.get(LOCATION_KEY));
+			}
+		}
+		
 		//if the developer explicitly set that is COOPA compliant in the config file 
 		//then we set it as true otherwise we don't set it and it will be false by default;
 		if(isCOPPAcompliant()){
@@ -210,5 +247,61 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 		
 		return  Boolean.parseBoolean(coppaCompliant);
 	}
-	
+
+
+	private Map<String, Object> getRuntimeMetadata(){
+		Map<String, Object> configurationForAdapter = SPMediationConfigurator.INSTANCE.getConfigurationForAdapter("AdMob");
+		
+		if(getGender() != null){
+			configurationForAdapter.put(GENDER_KEY, getGender());
+		}
+		
+		if(getDate() != null){
+			configurationForAdapter.put(BIRTHDAY_KEY, getDate());
+		}
+		
+		if(getLocation() != null){
+			configurationForAdapter.put(LOCATION_KEY, getLocation());
+		}
+		
+		return configurationForAdapter;
+	}
+
+	public static Integer getGender() {
+		return gender;
+	}
+
+	/**
+	 * 
+	 * @param gender - use the static fields of the AdRequest class
+	 *                 GENDER_FEMALE(=2), GENDER_MALE(=1), GENDER_UNKNOWN(=0)
+	 * @return
+	 */
+	public static void setGender(Integer gender) {
+		AdMobIntersitialMediationAdapter.gender = gender;
+	}
+
+	public static Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date - the selected date
+	 * @return
+	 */
+	public static void setDate(Date date) {
+		AdMobIntersitialMediationAdapter.date = date;
+	}
+
+	/**
+	 * @param location - the selected location
+	 * @return
+	 */
+	public static Location getLocation() {
+		return location;
+	}
+
+	public static void setLocation(Location location) {
+		AdMobIntersitialMediationAdapter.location = location;
+	}
 }

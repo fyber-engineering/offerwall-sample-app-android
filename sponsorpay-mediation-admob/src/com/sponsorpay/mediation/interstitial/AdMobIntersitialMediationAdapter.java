e240bc687b9867424ae51999c074d1f33bbe40d6
alexakios <alexios.karapetsas@sponsorpay.com>
2014-04-01 17:31:50 +0200

    I have implemented the changes on AdMob adaptor according to David's suggestions.

--- sponsorpay-mediation-admob/src/com/sponsorpay/mediation/interstitial/AdMobIntersitialMediationAdapter.java
+++ sponsorpay-mediation-admob/src/com/sponsorpay/mediation/interstitial/AdMobIntersitialMediationAdapter.java
@@ -7,7 +7,6 @@
 package com.sponsorpay.mediation.interstitial;
 
 import java.util.Date;
-import java.util.Map;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,15 +33,15 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 	 * to set manual settings for the request adapter.
 	 */
 	private static final String BUILDER_CONFIG_ADD_TEST_DEVICE = "addTestDevice";
-	private static final String COOPA_COPLIANT                 = "isCOPPAcompliant";
+	private static final String COOPA_COMPLIANT                 = "isCOPPAcompliant";
 		
 	/**
 	 * The following keys are declared in the config file and are used
 	 * to set the runtime settings for the request adapter.
 	 */
-	private static final String BIRTHDAY_KEY   = "birthday";
-	private static final String GENDER_KEY     = "gender";
-	private static final String LOCATION_KEY   = "location";
+	public static final String BIRTHDAY_KEY   = "birthday";
+	public static final String GENDER_KEY     = "gender";
+	public static final String LOCATION_KEY   = "location";
 	
 	/**
 	 * These are the 4 possible errors that we can take back when we are 
@@ -53,10 +52,6 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 	private static final String NETWORK_ERROR   = "ERROR_CODE_NETWORK_ERROR";
 	private static final String CODE_NO_FILL    = "ERROR_CODE_NO_FILL";
 	
-	private static Integer gender;
-	private static Date date;
-	private static Location location;
-	
 	
 	private InterstitialAd interstitial;
 
@@ -66,8 +61,8 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 		// Create the interstitial.
 	    interstitial = new InterstitialAd(activity);
 	    
-	    // Our ad unit id. Could be replaced with our actual ad unit id (could be either Home or Level up). 
-	    String adUnitId = mAdapter.addUnitId();
+	    // Our ad unit id. Could be either Home or Level up. Can be change on the config file. 
+	    String adUnitId = mAdapter.getAdUnitId();
 	    interstitial.setAdUnitId(adUnitId);
 	    
 	    
@@ -159,7 +154,7 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 		
 		
 		// Get the list of the test device ids.
-		JSONArray metadata = getConfigListOfDevices();
+		JSONArray testDevices = getConfigListOfDevices();
 		
 		
 		//  Set the emulator as a default test device
@@ -167,12 +162,12 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 		
 		
 		// Set the ids of all testing devices that have been declared in the config file.
-		if (metadata != null) {
-			if (metadata.length() > 0) {
-				for (int i = 0; i < metadata.length(); i++) {
+		if (testDevices != null) {
+			if (testDevices.length() > 0) {
+				for (int i = 0; i < testDevices.length(); i++) {
 					String deviceId;
 					try {
-						deviceId = metadata.getJSONObject(i).toString();
+						deviceId = testDevices.getString(i);
 						requestBuilder.addTestDevice(deviceId);
 					} catch (JSONException jsonexc) {
 						SponsorPayLogger.e(TAG, "Error on parsing device id.");
@@ -182,30 +177,35 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 		}
 		
 		
-		//set all the runtime configuration for gender, location and date
-		//which will be used for ad targeting
+		/**
+		 * set all the runtime configuration for gender, 
+		 * location and date which will be used for 
+		 * ad targeting
+		*/
 		
-		Map<String, Object> runtimeConfigurationForAdapter = getRuntimeMetadata();
-		if (!runtimeConfigurationForAdapter.isEmpty()) {
-			if (runtimeConfigurationForAdapter.containsKey(GENDER_KEY)) {
-
-				requestBuilder.setGender((Integer) runtimeConfigurationForAdapter.get(GENDER_KEY));
-			}
-
-			if (runtimeConfigurationForAdapter.containsKey(BIRTHDAY_KEY)) {
-
-				requestBuilder.setBirthday((Date) runtimeConfigurationForAdapter.get(BIRTHDAY_KEY));
-			}
-
-			if (runtimeConfigurationForAdapter.containsKey(LOCATION_KEY)) {
-
-				requestBuilder.setLocation((Location) runtimeConfigurationForAdapter.get(LOCATION_KEY));
-			}
+		
+		//There are 3 possible cases for the gender
+		//GENDER_FEMALE(=2), GENDER_MALE(=1), GENDER_UNKNOWN(=0)
+		//It can be accessed with the a static way from the
+		//AdRequest class, i.e. AdRequest.GENDER_FEMALE
+		Integer gender = SPMediationConfigurator.getConfiguration(getName(), GENDER_KEY, Integer.class);
+		if (gender != null) {
+			requestBuilder.setGender(gender);
+		}
+		
+		Date birthday = SPMediationConfigurator.getConfiguration(getName(), BIRTHDAY_KEY, Date.class);
+		if (birthday != null) {
+			requestBuilder.setBirthday(birthday);
+		}
+		
+		Location location = SPMediationConfigurator.getConfiguration(getName(), LOCATION_KEY, Location.class);
+		if (location != null) {
+			requestBuilder.setLocation(location);
 		}
 		
 		//if the developer explicitly set that is COOPA compliant in the config file 
 		//then we set it as true otherwise we don't set it and it will be false by default;
-		if(isCOPPAcompliant()){
+		if(isCOPPACompliant()){
 			requestBuilder.tagForChildDirectedTreatment(true);
 		}
 		
@@ -242,66 +242,10 @@ public class AdMobIntersitialMediationAdapter extends SPInterstitialMediationAda
 	/**
 	 * @return true- if it's COOPA compliant otherwise false.
 	 */
-	private boolean isCOPPAcompliant(){
-		String coppaCompliant = SPMediationConfigurator.getConfiguration(getName(), COOPA_COPLIANT, String.class);
+	private boolean isCOPPACompliant(){
+		String coppaCompliant = SPMediationConfigurator.getConfiguration(getName(), COOPA_COMPLIANT, String.class);
 		
 		return  Boolean.parseBoolean(coppaCompliant);
 	}
 
-
-	private Map<String, Object> getRuntimeMetadata(){
-		Map<String, Object> configurationForAdapter = SPMediationConfigurator.INSTANCE.getConfigurationForAdapter("AdMob");
-		
-		if(getGender() != null){
-			configurationForAdapter.put(GENDER_KEY, getGender());
-		}
-		
-		if(getDate() != null){
-			configurationForAdapter.put(BIRTHDAY_KEY, getDate());
-		}
-		
-		if(getLocation() != null){
-			configurationForAdapter.put(LOCATION_KEY, getLocation());
-		}
-		
-		return configurationForAdapter;
-	}
-
-	public static Integer getGender() {
-		return gender;
-	}
-
-	/**
-	 * 
-	 * @param gender - use the static fields of the AdRequest class
-	 *                 GENDER_FEMALE(=2), GENDER_MALE(=1), GENDER_UNKNOWN(=0)
-	 * @return
-	 */
-	public static void setGender(Integer gender) {
-		AdMobIntersitialMediationAdapter.gender = gender;
-	}
-
-	public static Date getDate() {
-		return date;
-	}
-
-	/**
-	 * @param date - the selected date
-	 * @return
-	 */
-	public static void setDate(Date date) {
-		AdMobIntersitialMediationAdapter.date = date;
-	}
-
-	/**
-	 * @param location - the selected location
-	 * @return
-	 */
-	public static Location getLocation() {
-		return location;
-	}
-
-	public static void setLocation(Location location) {
-		AdMobIntersitialMediationAdapter.location = location;
-	}
 }

7db8e109ff6265982f1ae876fea626ea4def57af
hubert <hubert.kosacki@codete.co>
2014-05-28 18:37:38 +0200

    approach to put some facebook sdk methods into adapter

--- sponsorpay-mediation-facebook/src/com/sponsorpay/mediation/FacebookMediationAdapter.java
+++ sponsorpay-mediation-facebook/src/com/sponsorpay/mediation/FacebookMediationAdapter.java
@@ -4,10 +4,25 @@ import java.util.Set;
 
 import android.app.Activity;
 
+import com.sponsorpay.mediation.interstitial.FacebookInterstitialMediationAdapter;
 import com.sponsorpay.publisher.interstitial.mediation.SPInterstitialMediationAdapter;
 import com.sponsorpay.publisher.mbe.mediation.SPBrandEngageMediationAdapter;
 
 public class FacebookMediationAdapter extends SPMediationAdapter {
+
+	private static final String TAG = FacebookMediationAdapter.class.getSimpleName();
+	private static final String ADAPTER_VERSION = "0.1";
+
+	private static final String ADAPTER_NAME = "Facebook";
+	private FacebookInterstitialMediationAdapter mInterstitialAdapter;
+
+	/**
+	 * Initializes the wrapped SDK, usually with the necessary credentials.
+	 * 
+	 * Parameters: activity - The parent activity calling this method
+	 * 
+	 * Returns: true if the adapter was successfully started, false otherwise
+	 */
 
 	@Override
 	public boolean startAdapter(Activity activity) {
@@ -15,12 +30,18 @@ public class FacebookMediationAdapter extends SPMediationAdapter {
 		return false;
 	}
 
+	/**
+	 * Returns:the name of the wrapped network.
+	 */
 	@Override
 	public String getName() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * Returns:the version of the wrapped network.
+	 */
 	@Override
 	public String getVersion() {
 		// TODO Auto-generated method stub
@@ -33,6 +54,10 @@ public class FacebookMediationAdapter extends SPMediationAdapter {
 		return null;
 	}
 
+	/**
+	 * Returns the InterstitialAdapter which has been instantiated in
+	 * startAdapter(Activity)
+	 */
 	@Override
 	public SPInterstitialMediationAdapter<? extends SPMediationAdapter> getInterstitialMediationAdapter() {
 		// TODO Auto-generated method stub

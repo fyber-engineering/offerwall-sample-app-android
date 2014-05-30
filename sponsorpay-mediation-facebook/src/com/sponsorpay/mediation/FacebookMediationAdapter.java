073a046bbf220c7a6c89a2f6d7062aeefa50e5d3
hubert <hubert.kosacki@codete.co>
2014-05-30 17:42:56 +0200

    facebook adapter is right now a stub that is actually bound as Appia. Includes gradle scripts in each of the projects.

--- sponsorpay-mediation-facebook/src/com/sponsorpay/mediation/FacebookMediationAdapter.java
+++ sponsorpay-mediation-facebook/src/com/sponsorpay/mediation/FacebookMediationAdapter.java
@@ -7,14 +7,19 @@ import android.app.Activity;
 import com.sponsorpay.mediation.interstitial.FacebookInterstitialMediationAdapter;
 import com.sponsorpay.publisher.interstitial.mediation.SPInterstitialMediationAdapter;
 import com.sponsorpay.publisher.mbe.mediation.SPBrandEngageMediationAdapter;
+import com.sponsorpay.utils.SponsorPayLogger;
 
 public class FacebookMediationAdapter extends SPMediationAdapter {
 
 	private static final String TAG = FacebookMediationAdapter.class.getSimpleName();
-	private static final String ADAPTER_VERSION = "0.1";
+	private static final String ADAPTER_VERSION = "1.0.0";
 
-	private static final String ADAPTER_NAME = "Facebook";
+	private static final String ADAPTER_NAME = "Appia";
+//	private static final String ADAPTER_NAME = "Facebook";
 	private FacebookInterstitialMediationAdapter mInterstitialAdapter;
+
+	private static final String PLACEMENT_ID_KEY = "placementId";
+	private static final String TEST_DEVICE_HASH_KEY = "testDeviceHash";
 
 	/**
 	 * Initializes the wrapped SDK, usually with the necessary credentials.
@@ -26,8 +31,10 @@ public class FacebookMediationAdapter extends SPMediationAdapter {
 
 	@Override
 	public boolean startAdapter(Activity activity) {
-		// TODO Auto-generated method stub
-		return false;
+		// TODO do work here
+		SponsorPayLogger.d(TAG, "Starting Facebook Advertising SDK.");
+		mInterstitialAdapter = new FacebookInterstitialMediationAdapter(this, activity);
+		return true;
 	}
 
 	/**
@@ -35,8 +42,7 @@ public class FacebookMediationAdapter extends SPMediationAdapter {
 	 */
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return ADAPTER_NAME;
 	}
 
 	/**
@@ -44,13 +50,12 @@ public class FacebookMediationAdapter extends SPMediationAdapter {
 	 */
 	@Override
 	public String getVersion() {
-		// TODO Auto-generated method stub
-		return null;
+		return ADAPTER_VERSION;
 	}
 
 	@Override
 	public SPBrandEngageMediationAdapter<? extends SPMediationAdapter> getVideoMediationAdapter() {
-		// TODO Auto-generated method stub
+		// there is no video adapter
 		return null;
 	}
 
@@ -60,14 +65,20 @@ public class FacebookMediationAdapter extends SPMediationAdapter {
 	 */
 	@Override
 	public SPInterstitialMediationAdapter<? extends SPMediationAdapter> getInterstitialMediationAdapter() {
-		// TODO Auto-generated method stub
-		return null;
+		return mInterstitialAdapter;
 	}
 
 	@Override
 	protected Set<? extends Object> getListeners() {
-		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public String getPlacementId() {
+		return SPMediationConfigurator.getConfiguration(ADAPTER_NAME, PLACEMENT_ID_KEY, String.class);
+	}
+
+	public String getTestDeviceId() {
+		return SPMediationConfigurator.getConfiguration(ADAPTER_NAME, TEST_DEVICE_HASH_KEY, String.class);
 	}
 
 }

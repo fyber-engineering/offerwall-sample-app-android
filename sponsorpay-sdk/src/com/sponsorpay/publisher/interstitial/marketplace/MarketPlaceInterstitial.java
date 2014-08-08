82301b9675b2d16fa0bf2c6793f6360194f59d09
alexakios <alexios.karapetsas@sponsorpay.com>
2014-08-08 10:53:16 +0200

    Added close button, close and click events

--- sponsorpay-sdk/src/com/sponsorpay/publisher/interstitial/marketplace/MarketPlaceInterstitial.java
+++ sponsorpay-sdk/src/com/sponsorpay/publisher/interstitial/marketplace/MarketPlaceInterstitial.java
@@ -8,13 +8,28 @@ package com.sponsorpay.publisher.interstitial.marketplace;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.sponsorpay.mediation.marketplace.MarketPlaceAdapter;
 import com.sponsorpay.publisher.interstitial.SPInterstitialAd;
@@ -30,6 +45,7 @@ public class MarketPlaceInterstitial extends
 	private Handler mMainHandler;
 	private WebView mWebView;
 	private WebViewClient mWebClient;
+	private FrameLayout mainLayout;
 
 	private Activity mActivity;
 
@@ -42,6 +58,7 @@ public class MarketPlaceInterstitial extends
 				case CREATE_WEBVIEW:
 					MessageInfoHolder holder = (MessageInfoHolder) msg.obj;
 					mWebView = new WebView(holder.mContext);
+					createCloseButton(holder.mContext);
 					mWebView.getSettings().setJavaScriptEnabled(true);
 					mWebView.setWebViewClient(getWebClient());	
 //					loadHtml(holder.mHtml);
@@ -91,9 +108,8 @@ public class MarketPlaceInterstitial extends
 //		parentActivity.setContentView(frameLayout, new LayoutParams(
 //				LayoutParams.FILL_PARENT,
 //				LayoutParams.FILL_PARENT));
-		parentActivity.addContentView(mWebView, new LayoutParams(
-				LayoutParams.FILL_PARENT,
-				LayoutParams.FILL_PARENT));
+		FrameLayout.LayoutParams layoutparams= new FrameLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);            
+		parentActivity.addContentView(mainLayout, layoutparams);
 		return true;
 	}
 
@@ -138,8 +154,95 @@ public class MarketPlaceInterstitial extends
 
 		private MessageInfoHolder(Context context, String html) {
 			this.mContext = context;
-			this.mHtml = html;
-			
+			this.mHtml = html;			
 		}
+	}
+	
+	private void createCloseButton(Context context){
+
+		mainLayout = new FrameLayout(context);
+		
+		//RelativeLayout childLayout = new RelativeLayout(context);
+		RelativeLayout childLayout = new RelativeLayout(context);
+
+		//Image with drawable
+		final ImageView imageView;
+		imageView = new ImageView(context);
+		// create a drawable
+		ShapeDrawable circle = new ShapeDrawable(new OvalShape());
+		circle.setIntrinsicHeight(80);
+		circle.setIntrinsicWidth(80);
+		circle.setBounds(new Rect(0, 0, 40, 40));
+		circle.getPaint().setColor(Color.DKGRAY);
+		// set the drawable into the imageviw
+		imageView.setImageDrawable(circle);
+		imageView.setAdjustViewBounds(true);
+		imageView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		imageView.setPadding(5, 5, 5, 5);
+		imageView.setScaleType(ScaleType.FIT_XY);
+		
+		
+
+		// X close text
+		TextView text = new TextView(context);
+		text.setText("X");
+		text.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+		text.setTextColor(Color.WHITE);
+		RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+		text.setLayoutParams(params);
+		
+		
+		childLayout.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.TOP|Gravity.RIGHT));
+		childLayout.addView(imageView);
+		childLayout.addView(text);
+		
+		
+		//the webview
+		mWebView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+
+		mainLayout.addView(mWebView);
+		mainLayout.addView(childLayout);
+		//mainLayout.addView(text);
+
+	     
+	     
+	     /**
+	      * Listeners for calling the close and click events.
+	      * 
+	      */
+		imageView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				fireCloseEvent();
+				
+				if (mainLayout != null) {
+		            ViewGroup parentViewGroup = (ViewGroup) mainLayout.getParent();
+		            if (parentViewGroup != null) {
+		                parentViewGroup.removeAllViews();
+		            }
+		        }
+			}
+		});
+	     
+	     
+		mWebView.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+                fireClickEvent();
+				
+				if (mainLayout != null) {
+		            ViewGroup parentViewGroup = (ViewGroup) mainLayout.getParent();
+		            if (parentViewGroup != null) {
+		                parentViewGroup.removeAllViews();
+		            }
+		        }
+				return true;
+			}
+		});
+		
+		 
 	}
 }
